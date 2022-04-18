9f60df987a172e711e46eab4b513f31fad0e9090
mihailb22 <85743858+mihailb22@users.noreply.github.com>
2022-04-18 17:49:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+/* 
+https://teachablemachine.withgoogle.com/models/95qdeO7ae/
+*/
+
+
+Webcam.set({
+    width: 420,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
+
+    });
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/95qdeO7ae/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("model loaded");
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log("error");
+    } else {
+        console.log(results);
+        document.getElementById("result_person_name").innerHTML = results[0].label;
+        document.getElementById("result_person_accuracy").innerHTML = (results[0].confidence * 100).toFixed(3);
+    }
+}
